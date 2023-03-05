8141de2983be782a3712abd58bfced143a21f3c8
Matthew Wang <matt@matthewwang.me>
2023-03-05 12:02:35 -0800

    adds tailwind!

--- src/App.js
+++ src/App.js
@@ -58,8 +58,8 @@ function App() {
       return <div>no brews yet!</div>
     }
     return (
-      <ul className='run-list'>
-        {responses.map((resp, i) => <li className='single-run' key={resp.program + i}>
+      <ul className='p-0 list-none'>
+        {responses.map((resp, i) => <li className='single-run border-t py-2 text-ellipsis overflow-hidden whitespace-nowrap' key={resp.program + i}>
           brew #{resp.iteration} | <button onClick={() => loadProgram(resp.program, resp.output, resp.version)}>load</button>
           <br />
           in: {resp.program}
@@ -72,49 +72,47 @@ function App() {
 
   return (
     <div className="App">
-      <div className="container">
-        <main className='main-container'>
-          <div></div> {/* empty div for grid */}
+      <main className='main-container'>
+        <div></div> {/* empty div for grid */}
+        <div>
           <header>
-            <h1 className='inline'>☕ barista</h1>
-              &nbsp;
-              <p className='inline'>| brewin as a service</p>
-              <hr />
-              <select className="btn btn-blue-outline mr-0-5" style={{paddingLeft: '0.25rem'}} value={version} onChange={(e) => setVersion(e.target.value)}>
-                <option value="1">fall 2022</option>
-              </select>
-              <select className="btn btn-blue-outline mr-0-5" style={{paddingLeft: '0.25rem'}} value={version} onChange={(e) => setVersion(e.target.value)}>
-                <option value="1">1: brewin</option>
-                <option value="2">2: brewin++</option>
-                <option value="3">3: brewin#</option>
-              </select>
-              <button className="btn btn-blue" onClick={runProgram}>run</button>
-            </header>
-          <div>
-            <h2>past brews</h2>
-            <PastBrews responses={responses} loadProgram={loadProgram} />
-          </div>
-          <div>
-            <h2>your recipe</h2>
-            <Editor
-              className='editor'
-              value={program}
-              onValueChange={program => setProgram(program)}
-              highlight={code => code /* this is an identity -- no highlighting */}
-              padding={10}
-            />
+            <h1 className='text-2xl'><span className='font-bold'>☕ barista</span> | brewin as a service</h1>
+          </header>
+          <hr className='my-2' />
+          <select className="btn btn-blue-outline mr-1 pl-1">
+            <option value="1">fall 2022</option>
+          </select>
+          <select className="btn btn-blue-outline mr-1 pl-1" value={version} onChange={(e) => setVersion(e.target.value)}>
+            <option value="1">1: brewin</option>
+            <option value="2">2: brewin++</option>
+            <option value="3">3: brewin#</option>
+          </select>
+          <button className="btn btn-blue" onClick={runProgram}>brew!</button>
+        </div>
+        <div>
+          <h2 className='text-xl mb-1'>past brews</h2>
+          <PastBrews responses={responses} loadProgram={loadProgram} />
+        </div>
+        <div>
+          <h2 className='text-xl'>your recipe</h2>
+          <Editor
+            className='editor border'
+            value={program}
+            onValueChange={program => setProgram(program)}
+            highlight={code => code /* this is an identity -- no highlighting */}
+            padding={10}
+          />
 
-            <h2>your brew</h2>
-            <Editor
-              className='editor'
-              value={output}
-              highlight={code => code /* this is an identity -- no highlighting */}
-              padding={10}
-              readOnly={true}
-            />
-          </div>
-        </main>
-      </div>
+          <h2 className='text-xl'>your brew</h2>
+          <Editor
+            className='editor border'
+            value={output}
+            highlight={code => code /* this is an identity -- no highlighting */}
+            padding={10}
+            readOnly={true}
+          />
+        </div>
+      </main>
     </div>
   );
 }
